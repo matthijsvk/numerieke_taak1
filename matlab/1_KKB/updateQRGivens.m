c97de350229a8928bbf6682dc9deec69737f6b09
Matthijs Van keirsbilck <matthijs.vankeirsbilck@student.kuleuven.be>
2015-04-09 12:52:38 +0200

    cleaned up stuff

--- matlab/1_KKB/updateQRGivens.m
+++ matlab/1_KKB/updateQRGivens.m
@@ -6,21 +6,3 @@ function [Q,R] = updateQRGivens(A,Q,R)    %new A, old Q, old R
         %display('after insertion:');Q*R;
     end
 end
-
-% function [Q,R] = qrgivensupdate(A,Q,R) 
-%   [m,n] = size(A);
-%   [oldm,oldn]=size(Q)
-%   Q = [Q,zeros(oldm, m-oldm);zeros(m-oldm,oldn),eye(m - oldm)]   %Q = mxm
-%   R = A    %R= mxn
-% 
-%   for j = 1:n    %update all columns
-%       j
-%     for i = m:-1:(j+1)
-%       G = [eye(m)];%,zeros(m,m - oldm)];
-%       [c,s] = givensrotation( R(i-1,j),R(i,j) );
-%       G([i-1, i],[i-1, i]) = [c -s; s c]
-%       R = G'*R
-%       Q = Q*G
-%     end
-%   end
-% end
